3c61c6a7ed021db29ce9f271e636885fa349014f
GerardoDepazCruz <creugeni12@gmail.com>
2025-11-06 11:02:59 -0500

    Corregir importación de función multiplicacion

--- multiplicacion.test.js
+++ multiplicacion.test.js
@@ -1,5 +1,5 @@
-const { multiplicacion } = require('./math');
+const { multiplicacion } = require('./multiplicacion');
 
 test('multiplicacion de 2 * 3 debe ser 6', () => {
-    expect(multiplicacion(2, 3)).toBe(6);
+  expect(multiplicacion(2, 3)).toBe(6);
 });
