353b07b0d19b1e1962b10dc1ec9452498a7a3fdc
GerardoDepazCruz <creugeni12@gmail.com>
2025-11-13 11:10:41 -0500

    cambio de titulo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola desde Render y GitHub Actions!');
+  res.send('¡Hola desde Render soy Gerardo Thomas Depaz Cruz - ;) !');
 });
 
 app.listen(port, () => {
